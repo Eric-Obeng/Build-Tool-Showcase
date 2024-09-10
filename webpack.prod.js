5823307c77948daec0cf06b789cf3d93ffe95bc6
Eric-Obeng <obengeric.3958@gmail.com>
2024-09-10 11:22:25 +0000

    web pack for production

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,4 +1,9 @@
+const path = require("path");
+const { merge } = require("webpack-merge");
+const common = require("./webpack.common.js");
 const CssMinimizerPlugin = require("css-minimizer-webpack-plugin");
+const { CleanWebpackPlugin } = require("clean-webpack-plugin"); // Updated import
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
@@ -6,7 +11,7 @@ module.exports = merge(common, {
     filename: "[name].[contenthash].js", // File hashing for better caching
     path: path.resolve(__dirname, "dist"),
   },
-  plugins: [new CleanWebpackPlugin()],
+  plugins: [new CleanWebpackPlugin()], // Updated usage
   optimization: {
     splitChunks: {
       chunks: "all", // This will split all types of chunks (both dynamic and initial)
